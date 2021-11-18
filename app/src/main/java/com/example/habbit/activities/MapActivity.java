d938d01fd6ebbec68a13f524b7ecb3e3567e1018
SeanWilliamRutherford <swruther@ualberta.ca>
2021-11-18 12:21:36 -0700

    map stuff

---
+++ app/src/main/java/com/example/habbit/activities/MapActivity.java
@@ -0,0 +1,112 @@
+// Created by plusminus on 00:23:14 - 03.10.2008
+package com.example.habbit.activities;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.os.Bundle;
+import android.view.KeyEvent;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
+
+import com.example.habbit.R;
+import com.example.habbit.fragments.MapFragment;
+
+/**
+ * Default map view activity.
+ *
+ * @author Manuel Stahl
+ */
+public class MapActivity extends AppCompatActivity {
+    private static final String MAP_FRAGMENT_TAG = "org.osmdroid.MAP_FRAGMENT_TAG";
+
+    /**
+     * The idea behind that is to force a MapView refresh when switching from offline to online.
+     * If you don't do that, the map may display - when online - approximated tiles
+     * * that were computed when offline
+     * * that could be replaced by downloaded tiles
+     * * but as the display is not refreshed there's no try to get better tiles
+     *
+     * @since 6.0
+     */
+    private final BroadcastReceiver networkReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            try {
+                starterMapFragment.invalidateMapView();
+            } catch (NullPointerException e) {
+                // lazy handling of an improbable NPE
+            }
+        }
+    };
+
+    private MapFragment starterMapFragment;
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_map);
+
+        //custom toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //noinspection ConstantConditions
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        //MainActivity.updateStoragePreferences(this);    //needed for unit tests
+
+        registerReceiver(networkReceiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+
+        FragmentManager fm = this.getSupportFragmentManager();
+        if (fm.findFragmentByTag(MAP_FRAGMENT_TAG) == null) {
+            MapFragment starterMapFragment = MapFragment.newInstance();
+            fm.beginTransaction().add(R.id.map_container, starterMapFragment, MAP_FRAGMENT_TAG).commit();
+        }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    /**
+     * small example of keyboard events on the mapview
+     * page up = zoom out
+     * page down = zoom in
+     *
+     * @param keyCode
+     * @param event
+     * @return
+     */
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_PAGE_DOWN:
+                starterMapFragment.zoomIn();
+                return true;
+            case KeyEvent.KEYCODE_PAGE_UP:
+                starterMapFragment.zoomOut();
+                return true;
+        }
+        return super.onKeyUp(keyCode, event);
+    }
+
+    /**
+     * @since 6.0
+     */
+    @Override
+    protected void onDestroy() {
+        unregisterReceiver(networkReceiver);
+        super.onDestroy();
+    }
+}
---
+++ app/src/main/java/com/example/habbit/fragments/MapFragment.java
@@ -0,0 +1,284 @@
+// Created by plusminus on 00:23:14 - 03.10.2008
+package com.example.habbit.fragments;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.InputDevice;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.habbit.R;
+
+import org.osmdroid.api.IGeoPoint;
+import org.osmdroid.tileprovider.tilesource.ITileSource;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.CopyrightOverlay;
+import org.osmdroid.views.overlay.MinimapOverlay;
+import org.osmdroid.views.overlay.ScaleBarOverlay;
+import org.osmdroid.views.overlay.compass.CompassOverlay;
+import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
+import org.osmdroid.views.overlay.gestures.RotationGestureOverlay;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+
+/**
+ * Default map view activity.
+ *
+ * @author Marc Kurtz
+ * @author Manuel Stahl
+ */
+public class MapFragment extends Fragment {
+    // ===========================================================
+    // Constants
+    // ===========================================================
+
+    private static final String PREFS_NAME = "org.andnav.osm.prefs";
+    private static final String PREFS_TILE_SOURCE = "tilesource";
+    private static final String PREFS_LATITUDE_STRING = "latitudeString";
+    private static final String PREFS_LONGITUDE_STRING = "longitudeString";
+    private static final String PREFS_ORIENTATION = "orientation";
+    private static final String PREFS_ZOOM_LEVEL_DOUBLE = "zoomLevelDouble";
+
+    private static final int MENU_ABOUT = Menu.FIRST + 1;
+    private static final int MENU_LAST_ID = MENU_ABOUT + 1; // Always set to last unused id
+
+    // ===========================================================
+    // Fields
+    // ===========================================================
+    private SharedPreferences mPrefs;
+    private MapView mMapView;
+    private MyLocationNewOverlay mLocationOverlay;
+    private CompassOverlay mCompassOverlay = null;
+    private MinimapOverlay mMinimapOverlay;
+    private ScaleBarOverlay mScaleBarOverlay;
+    private RotationGestureOverlay mRotationGestureOverlay;
+    private CopyrightOverlay mCopyrightOverlay;
+
+    public static MapFragment newInstance() {
+        return new MapFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        //Note! we are programmatically construction the map view
+        //be sure to handle application lifecycle correct (see note in on pause)
+        mMapView = new MapView(inflater.getContext());
+        mMapView.setDestroyMode(false);
+        mMapView.setTag("mapView"); // needed for OpenStreetMapViewTest
+
+        mMapView.setOnGenericMotionListener(new View.OnGenericMotionListener() {
+            /**
+             * mouse wheel zooming ftw
+             * http://stackoverflow.com/questions/11024809/how-can-my-view-respond-to-a-mousewheel
+             * @param v
+             * @param event
+             * @return
+             */
+            @Override
+            public boolean onGenericMotion(View v, MotionEvent event) {
+                if (0 != (event.getSource() & InputDevice.SOURCE_CLASS_POINTER)) {
+                    switch (event.getAction()) {
+                        case MotionEvent.ACTION_SCROLL:
+                            if (event.getAxisValue(MotionEvent.AXIS_VSCROLL) < 0.0f)
+                                mMapView.getController().zoomOut();
+                            else {
+                                //this part just centers the map on the current mouse location before the zoom action occurs
+                                IGeoPoint iGeoPoint = mMapView.getProjection().fromPixels((int) event.getX(), (int) event.getY());
+                                mMapView.getController().animateTo(iGeoPoint);
+                                mMapView.getController().zoomIn();
+                            }
+                            return true;
+                    }
+                }
+                return false;
+            }
+        });
+        return mMapView;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        final Context context = this.getActivity();
+        final DisplayMetrics dm = context.getResources().getDisplayMetrics();
+
+        mPrefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+
+
+        //My Location
+        //note you have handle the permissions yourself, the overlay did not do it for you
+        mLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(context), mMapView);
+        mLocationOverlay.enableMyLocation();
+        mMapView.getOverlays().add(this.mLocationOverlay);
+
+
+        //Mini map
+        mMinimapOverlay = new MinimapOverlay(context, mMapView.getTileRequestCompleteHandler());
+        mMinimapOverlay.setWidth(dm.widthPixels / 5);
+        mMinimapOverlay.setHeight(dm.heightPixels / 5);
+        mMapView.getOverlays().add(this.mMinimapOverlay);
+
+
+        //Copyright overlay
+        mCopyrightOverlay = new CopyrightOverlay(context);
+        //i hate this very much, but it seems as if certain versions of android and/or
+        //device types handle screen offsets differently
+        mMapView.getOverlays().add(this.mCopyrightOverlay);
+
+
+        //On screen compass
+        mCompassOverlay = new CompassOverlay(context, new InternalCompassOrientationProvider(context),
+                mMapView);
+        mCompassOverlay.enableCompass();
+        mMapView.getOverlays().add(this.mCompassOverlay);
+
+
+        //map scale
+        mScaleBarOverlay = new ScaleBarOverlay(mMapView);
+        mScaleBarOverlay.setCentred(true);
+        mScaleBarOverlay.setScaleBarOffset(dm.widthPixels / 2, 10);
+        mMapView.getOverlays().add(this.mScaleBarOverlay);
+
+
+        //support for map rotation
+        mRotationGestureOverlay = new RotationGestureOverlay(mMapView);
+        mRotationGestureOverlay.setEnabled(true);
+        mMapView.getOverlays().add(this.mRotationGestureOverlay);
+
+
+        //needed for pinch zooms
+        mMapView.setMultiTouchControls(true);
+
+        //scales tiles to the current screen's DPI, helps with readability of labels
+        mMapView.setTilesScaledToDpi(true);
+
+        //the rest of this is restoring the last map location the user looked at
+        final float zoomLevel = mPrefs.getFloat(PREFS_ZOOM_LEVEL_DOUBLE, 1);
+        mMapView.getController().setZoom(zoomLevel);
+        final float orientation = mPrefs.getFloat(PREFS_ORIENTATION, 0);
+        mMapView.setMapOrientation(orientation, false);
+        final String latitudeString = mPrefs.getString(PREFS_LATITUDE_STRING, "1.0");
+        final String longitudeString = mPrefs.getString(PREFS_LONGITUDE_STRING, "1.0");
+        final double latitude = Double.valueOf(latitudeString);
+        final double longitude = Double.valueOf(longitudeString);
+        mMapView.setExpectedCenter(new GeoPoint(latitude, longitude));
+
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onPause() {
+        //save the current location
+        final SharedPreferences.Editor edit = mPrefs.edit();
+        edit.putString(PREFS_TILE_SOURCE, mMapView.getTileProvider().getTileSource().name());
+        edit.putFloat(PREFS_ORIENTATION, mMapView.getMapOrientation());
+        edit.putString(PREFS_LATITUDE_STRING, String.valueOf(mMapView.getMapCenter().getLatitude()));
+        edit.putString(PREFS_LONGITUDE_STRING, String.valueOf(mMapView.getMapCenter().getLongitude()));
+        edit.putFloat(PREFS_ZOOM_LEVEL_DOUBLE, (float) mMapView.getZoomLevelDouble());
+        edit.commit();
+
+        mMapView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        //this part terminates all of the overlays and background threads for osmdroid
+        //only needed when you programmatically create the map
+        mMapView.onDetach();
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        final String tileSourceName = mPrefs.getString(PREFS_TILE_SOURCE,
+                TileSourceFactory.DEFAULT_TILE_SOURCE.name());
+        try {
+            final ITileSource tileSource = TileSourceFactory.getTileSource(tileSourceName);
+            mMapView.setTileSource(tileSource);
+        } catch (final IllegalArgumentException e) {
+            mMapView.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE);
+        }
+
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // Put overlay items first
+        mMapView.getOverlayManager().onCreateOptionsMenu(menu, MENU_LAST_ID, mMapView);
+
+        // Put "About" menu item last
+        menu.add(0, MENU_ABOUT, Menu.CATEGORY_SECONDARY, R.string.about).setIcon(
+                android.R.drawable.ic_menu_info_details);
+
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public void onPrepareOptionsMenu(final Menu pMenu) {
+        mMapView.getOverlayManager().onPrepareOptionsMenu(pMenu, MENU_LAST_ID, mMapView);
+        super.onPrepareOptionsMenu(pMenu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mMapView.getOverlayManager().onOptionsItemSelected(item, MENU_LAST_ID, mMapView)) {
+            return true;
+        }
+
+        switch (item.getItemId()) {
+            case MENU_ABOUT:
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity())
+                        .setTitle(R.string.app_name).setMessage(R.string.about_message)
+                        .setIcon(R.drawable.ic__192595)
+                        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int whichButton) {
+                                        //
+                                    }
+                                }
+                        );
+                builder.create().show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void zoomIn() {
+        mMapView.getController().zoomIn();
+    }
+
+    public void zoomOut() {
+        mMapView.getController().zoomOut();
+    }
+
+    // @Override
+    // public boolean onTrackballEvent(final MotionEvent event) {
+    // return this.mMapView.onTrackballEvent(event);
+    // }
+    public void invalidateMapView() {
+        mMapView.invalidate();
+    }
+}
